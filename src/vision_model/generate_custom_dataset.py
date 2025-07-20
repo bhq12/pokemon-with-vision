80f5ce4fa38b81751f77696276343259c10fb0f5
bhq12 <brook.queree@gmail.com>
2025-07-20 19:55:17 +1000

    Add label generation for custom dataset images

--- src/vision_model/generate_custom_dataset.py
+++ src/vision_model/generate_custom_dataset.py
@@ -4,8 +4,10 @@ import random
 import torch
 from torchvision.transforms import v2
 
+DATASET_PATH = "./custom_dataset"
+
 #TODO REMAINING:
-#- Generate bounding box labels on image creation
+#- Fix bounding box label format for the sprites on image creation 
 #- Add object sprites to the source data (houses, pokecenter, trees, water, etc.)
 #- Explore more transforms
 #- Generate train, test, and validate sets
@@ -29,10 +31,18 @@ def randomly_place_sprite(image, sprite):
 
     Image.Image.paste(image, sprite, (sprite_x, sprite_y))
 
+    return [
+        str(sprite_x - (sprite_width // 2)),
+        str(sprite_x + (sprite_width // 2)),
+        str(sprite_y - (sprite_height // 2)),
+        str(sprite_y + (sprite_height // 2))
+    ]
+
 
 def generate_new_image(file_number):
     new_image = Image.new("RGB", (144, 160), (255, 255, 255))
     selected_sprites = []
+    labels = []
     for i in range(int(3 * random.random())):
         # Open random sprite
         sprites = os.listdir('./pokemon_sprites')
@@ -51,10 +61,28 @@ def generate_new_image(file_number):
         transformed_sprite = transforms(sprite)
 
         # place the transformed sprite on the blank image
-        randomly_place_sprite(new_image, transformed_sprite)
+        label_location = randomly_place_sprite(new_image, transformed_sprite)
+        labels.append([selected_sprite] + label_location)
+
+    file_name = f"{file_number}_{'_'.join(selected_sprites)}"
+    image_path = f"{DATASET_PATH}/train/images/{file_name}.png"
+    label_path = f"{DATASET_PATH}/train/labels/{file_name}.txt"
+    new_image.save(image_path, "PNG")
+    label_file = open(label_path, 'w+')
+    for label in labels:
+        print(' '.join(label) + '\n')
+        label_file.write(' '.join(label) + '\n')
+    label_file.close()
+
+def setup_dataset_directories():
+    os.makedirs(f"{DATASET_PATH}/train/images", exist_ok=True)
+    os.makedirs(f"{DATASET_PATH}/train/labels", exist_ok=True)
+    os.makedirs(f"{DATASET_PATH}/test/images", exist_ok=True)
+    os.makedirs(f"{DATASET_PATH}/test/labels", exist_ok=True)
+    os.makedirs(f"{DATASET_PATH}/val/images", exist_ok=True)
+    os.makedirs(f"{DATASET_PATH}/val/labels", exist_ok=True)
 
 
-    new_image.save(f"./custom_dataset/train/{file_number}_{'_'.join(selected_sprites)}.png", "PNG")
-
+setup_dataset_directories()
 for i in range(100):
     generate_new_image(i)
